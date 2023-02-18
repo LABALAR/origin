40efa049bef065d1557d862fced2af1f35295b2c
Ser <arrioll1000@gmail.com>
2023-02-18 20:38:43 +0300

    classes

---
+++ classses/1/classes_1/classes_1.cpp
@@ -0,0 +1,107 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+class calculator
+{
+public:
+	double num1 = 0, num2 = 0;
+
+	double add(double num1, double num2)
+	{
+		return (num1 + num2);
+	}
+
+	double multiply(double num1, double num2)
+	{
+		return (num1 * num2);
+	}
+	double  subtract_1_2(double num1, double num2)
+	{
+		return (num1 - num2);
+	}
+	double subtract_2_1(double num1, double num2)
+	{
+		return (num2 - num1);
+	}
+	double divide_1_2(double num1, double num2)
+	{
+		return(num1 / num2);
+	}
+	double divide_2_1(double num1, double num2)
+	{
+		return(num2 / num1);
+	}
+	bool set_num1(double num1)
+	{
+			if (num1 != 0)
+			{
+				this->num1 = num1;
+				return(true);
+			}
+			else
+			{
+				return (false);
+			}
+
+	}
+	bool set_num2(double num2)
+	{
+			if (num2 !=0)
+			{
+				this->num2 = num2;
+				return(true);
+			}
+			else
+			{
+				return (false);
+			}
+	}
+
+
+};
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "Russian");
+
+	calculator calc;
+	while (1)
+	{
+		do
+		{
+			cout << "Введите первое число :";
+			cin >> calc.num1;
+			calc.set_num1(calc.num1);
+			
+				if (calc.num1 == 0)
+				{
+					cout << "Неверный ввод!" << endl;
+				}
+		} while(calc.num1 == 0);
+			do
+			{
+				cout << "Введите второе число :";
+				cin >> calc.num2;
+				calc.set_num2(calc.num2);
+					if (calc.num2 == 0)
+					{
+						cout << "Неверный ввод!" << endl;
+					}
+			} while (calc.num2 == 0);
+
+			cout << "num1 + num2 = " << calc.add(calc.num1, calc.num2) << endl;
+			cout << "num1 * num2 = " << calc.multiply(calc.num1, calc.num2) << endl;
+			cout << "num1/num2 = " << calc.divide_1_2(calc.num1, calc.num2) << endl;
+			cout << "num2/num1 = " << calc.divide_2_1(calc.num1, calc.num2) << endl;
+			cout << "num1 - num2 = " << calc.subtract_1_2(calc.num1, calc.num2) << endl;
+			cout << "num2 - num1 = " << calc.subtract_2_1(calc.num1, calc.num2) << endl;
+
+	}
+
+
+
+	return 0;
+}
---
+++ classses/2/scetchik/scetchik.cpp
@@ -0,0 +1,133 @@
+﻿#include <iostream>
+//#include <windows.h>
+#include <cstdlib>
+
+using namespace std;
+
+
+
+class Counter
+{
+private:
+    int num;
+
+public:
+
+    Counter()
+    {
+        num = 1;
+    }
+
+    Counter(int num)
+    {
+
+    }
+
+    int plus(int num)
+    {
+        num++;
+        return num;
+    }
+    int minus(int num)
+    {
+        num--;
+        return num;
+
+    }
+    int res(int num)
+    {
+        cout << num << endl;
+        return num;
+    }
+
+    int setnum(int t_num)
+    {
+        int  num = t_num;
+        return num;
+    }
+
+};
+
+int main()
+{
+    //  SetConsoleCP(1251);
+    //  SetConsoleOutputCP(1251);
+    //  setlocale(LC_ALL, "Russian");
+
+
+    int n = 0;
+    char command = 0;
+    string answer;
+    Counter count;
+
+    cout << "Do you want change num? Answer 'yes' or 'no':";
+
+
+    while (1)
+    {
+        cin >> answer;
+        if (answer == "yes")
+        {
+            cout << "THIS IS YES" << endl;
+            cin >> n;
+            count.setnum(n);
+            break;
+        }
+        else if (answer == "no")
+        {
+            cout << "THIS IS NO" << endl;
+            n = count.setnum(1);
+            break;
+        }
+        else
+        {
+            cout << "Error! Wrong answer! Try again :)" << endl;
+            continue;
+        }
+    }
+
+
+    while (1)
+    {
+        cout << "Vvedite '+', '-', '=', 'x':  ";
+        cin >> command;
+
+        switch (static_cast<int>(command))
+        {
+        case '+':
+        {
+            n = count.plus(n);
+            continue;
+        }
+        case '-':
+        {
+            n = count.minus(n);
+            continue;
+        }
+        case '=':
+        {
+            n = count.res(n);
+            continue;
+        }
+
+        default:
+        {
+            cout << "Error! Wrong command! Try again." << endl;
+            continue;
+        }
+
+        case 'x':
+        {
+            break;
+        }
+        }
+        break;
+    }
+
+
+    cout << "====End programm====" << endl;
+   
+
+
+    return 0;
+}
