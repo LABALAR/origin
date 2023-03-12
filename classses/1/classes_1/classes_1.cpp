9e4460eca81ba2115c98461e05784a64abd47c6f
Ser <arrioll1000@gmail.com>
2023-03-12 22:52:57 +0300

    classes_v2

--- classses/1/classes_1/classes_1.cpp
+++ classses/1/classes_1/classes_1.cpp
@@ -1,107 +1,117 @@
 ﻿#include <iostream>
-#include <windows.h>
+//#include <windows.h> 
 using namespace std;
+
 
 class calculator
 {
+private:
+    double num1 = 0, num2 = 0;
+
 public:
-	double num1 = 0, num2 = 0;
 
-	double add(double num1, double num2)
-	{
-		return (num1 + num2);
-	}
+    double add()
+    {
+        return (num1 + num2);
+    }
 
-	double multiply(double num1, double num2)
-	{
-		return (num1 * num2);
-	}
-	double  subtract_1_2(double num1, double num2)
-	{
-		return (num1 - num2);
-	}
-	double subtract_2_1(double num1, double num2)
-	{
-		return (num2 - num1);
-	}
-	double divide_1_2(double num1, double num2)
-	{
-		return(num1 / num2);
-	}
-	double divide_2_1(double num1, double num2)
-	{
-		return(num2 / num1);
-	}
-	bool set_num1(double num1)
-	{
-			if (num1 != 0)
-			{
-				this->num1 = num1;
-				return(true);
-			}
-			else
-			{
-				return (false);
-			}
+    double multiply()
+    {
+        return (num1 * num2);
+    }
 
-	}
-	bool set_num2(double num2)
-	{
-			if (num2 !=0)
-			{
-				this->num2 = num2;
-				return(true);
-			}
-			else
-			{
-				return (false);
-			}
-	}
+    double subtract_1_2()
+    {
+        return (num1 - num2);
+    }
 
+    double subtract_2_1()
+    {
+        return (num2 - num1);
+    }
+
+    double divide_1_2()
+    {
+        return (num1 / num2);
+    }
+
+    double divide_2_1()
+    {
+        return (num2 / num1);
+    }
+
+    bool set_num1(double num1)
+    {
+        if (num1 != 0)
+        {
+            this->num1 = num1;
+            return (true);
+        }
+        else
+        {
+            return (false);
+        }
+    }
+
+    bool set_num2(double num2)
+    {
+        if (num2 != 0)
+        {
+            this->num2 = num2;
+            return (true);
+        }
+        else
+        {
+            return (false);
+        }
+    }
 
 };
 
+
 int main()
 {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	setlocale(LC_ALL, "Russian");
+    //SetConsoleCP(1251);
+    //SetConsoleOutputCP(1251);
+    //setlocale(LC_ALL, "Russian");
+    double num;
+    calculator calc;
 
-	calculator calc;
-	while (1)
-	{
-		do
-		{
-			cout << "Введите первое число :";
-			cin >> calc.num1;
-			calc.set_num1(calc.num1);
-			
-				if (calc.num1 == 0)
-				{
-					cout << "Неверный ввод!" << endl;
-				}
-		} while(calc.num1 == 0);
-			do
-			{
-				cout << "Введите второе число :";
-				cin >> calc.num2;
-				calc.set_num2(calc.num2);
-					if (calc.num2 == 0)
-					{
-						cout << "Неверный ввод!" << endl;
-					}
-			} while (calc.num2 == 0);
+    while (1)
+    {
+        do
+        {
+            cout << "Vvedite 1 chislo: ";
+            cin >> num;
+            calc.set_num1(num);
+            if (calc.set_num1(num) == false)
+            {
+                cout << "Error!" << endl;
+            }
 
-			cout << "num1 + num2 = " << calc.add(calc.num1, calc.num2) << endl;
-			cout << "num1 * num2 = " << calc.multiply(calc.num1, calc.num2) << endl;
-			cout << "num1/num2 = " << calc.divide_1_2(calc.num1, calc.num2) << endl;
-			cout << "num2/num1 = " << calc.divide_2_1(calc.num1, calc.num2) << endl;
-			cout << "num1 - num2 = " << calc.subtract_1_2(calc.num1, calc.num2) << endl;
-			cout << "num2 - num1 = " << calc.subtract_2_1(calc.num1, calc.num2) << endl;
-
-	}
+        } while (num == 0);
 
 
+        do
+        {
+            cout << "Vvedite 2 chislo: ";
+            cin >> num;
+            calc.set_num2(num);
+            if (calc.set_num2(num) == false)
+            {
+                cout << "Error!" << endl;
+            }
 
-	return 0;
+        } while (num == 0);
+
+        cout << "num1 + num2 = " << calc.add() << endl;
+        cout << "num1 * num2 = " << calc.multiply() << endl;
+        cout << "num1 / num2 = " << calc.divide_1_2() << endl;
+        cout << "num2 / num1 = " << calc.divide_2_1() << endl;
+        cout << "num1 - num2 = " << calc.subtract_1_2() << endl;
+        cout << "num2 - num1 = " << calc.subtract_2_1() << endl;
+
+    }
+
+    return 0;
 }
--- classses/2/scetchik/scetchik.cpp
+++ classses/2/scetchik/scetchik.cpp
@@ -18,34 +18,33 @@ public:
         num = 1;
     }
 
-    Counter(int num)
+    Counter(int in_num)
     {
-
+        num = in_num;
     }
 
-    int plus(int num)
+    int plus()
     {
         num++;
         return num;
     }
-    int minus(int num)
+    int minus()
     {
         num--;
         return num;
-
     }
-    int res(int num)
+    int res()
     {
         cout << num << endl;
         return num;
     }
 
-    int setnum(int t_num)
-    {
-        int  num = t_num;
-        return num;
-    }
-
+    /*         int setnum(int t_num)
+             {
+                int  num = t_num;
+                return num;
+             }
+     */
 };
 
 int main()
@@ -55,10 +54,10 @@ int main()
     //  setlocale(LC_ALL, "Russian");
 
 
-    int n = 0;
+    int n = 0, a = 0;
     char command = 0;
     string answer;
-    Counter count;
+    //    Counter count;
 
     cout << "Do you want change num? Answer 'yes' or 'no':";
 
@@ -70,13 +69,102 @@ int main()
         {
             cout << "THIS IS YES" << endl;
             cin >> n;
-            count.setnum(n);
+            Counter count(n);
+
+            while (1)
+            {
+                cout << "Vvedite '+', '-', '=', 'x':  ";
+                cin >> command;
+
+                switch (static_cast<int>(command))
+                {
+                case '+':
+                {
+
+                    count.plus();
+                    continue;
+                }
+                case '-':
+                {
+
+                    count.minus();
+                    continue;
+                }
+                case '=':
+                {
+
+                    count.res();
+                    continue;
+                }
+
+                default:
+                {
+                    cout << "Error! Wrong command! Try again." << endl;
+                    continue;
+                }
+
+                case 'x':
+                {
+                    break;
+                }
+                }
+                break;
+            }
+
+
+
+
+
+            //          count.setnum(n);
             break;
         }
         else if (answer == "no")
         {
             cout << "THIS IS NO" << endl;
-            n = count.setnum(1);
+            Counter count;
+
+            while (1)
+            {
+                cout << "Vvedite '+', '-', '=', 'x':  ";
+                cin >> command;
+
+                switch (static_cast<int>(command))
+                {
+                case '+':
+                {
+                    count.plus();
+                    continue;
+                }
+                case '-':
+                {
+                    count.minus();
+                    continue;
+                }
+                case '=':
+                {
+                    count.res();
+                    continue;
+                }
+
+                default:
+                {
+                    cout << "Error! Wrong command! Try again." << endl;
+                    continue;
+                }
+
+                case 'x':
+                {
+                    break;
+                }
+                }
+                break;
+            }
+
+
+
+
+
+            //          n = count.setnum(1);          
             break;
         }
         else
@@ -86,48 +174,52 @@ int main()
         }
     }
 
-
+    /*
     while (1)
-    {
-        cout << "Vvedite '+', '-', '=', 'x':  ";
-        cin >> command;
+     {
+         cout << "Vvedite '+', '-', '=', 'x':  ";
+         cin >> command;
 
-        switch (static_cast<int>(command))
-        {
-        case '+':
-        {
-            n = count.plus(n);
-            continue;
-        }
-        case '-':
-        {
-            n = count.minus(n);
-            continue;
-        }
-        case '=':
-        {
-            n = count.res(n);
-            continue;
-        }
+         switch (static_cast<int>(command))
+         {
+             case '+':
+             {
+                 Counter (n++);
+ //                n = count.plus(n);
+                 continue;
+             }
+                     case '-':
+                     {
+                         Counter (n--);
+ //                        n = count.minus(n);
+                         continue;
+                     }
+                             case '=':
+                             {
+                                 count.res();
+ //                                n = count.res(n);
+                                 continue;
+                             }
 
-        default:
-        {
-            cout << "Error! Wrong command! Try again." << endl;
-            continue;
-        }
+                                     default:
+                                     {
+                                         cout << "Error! Wrong command! Try again." << endl;
+                                         continue;
+                                     }
 
-        case 'x':
-        {
-            break;
-        }
-        }
-        break;
-    }
-
+                                             case 'x':
+                                             {
+                                                 break;
+                                             }
+         }
+     break;
+     }
+     */
 
     cout << "====End programm====" << endl;
-   
+    cin >> a;
 
 
     return 0;
 }
+
